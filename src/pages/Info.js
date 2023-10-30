297724eb78370f40eb042e5b56240065d4d1bf40
FEDERICO SZNAJDERHAUS <47206175@ort>
2023-10-30 16:01:11 -0300

    30/10

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -6,7 +6,9 @@ export default function Home() {
       <div className="TituloLista">
       Info
       </div>
-      Me llamo Federico Sznajderhaus. Soy un estudiante de Ort, sede Almagro y soy alumno de informática. Soy muy bueno en todo lo relacionado en la matemática y en la lógica. Me gusta jugar al basket, los videojuegos y salir de fiesta. A la hora de trabajar, soy muy organizado, me destaco por mis destalles, siempre intento dar lo mejor de mí y poseo habilidades de liderazgo.
+      Me llamo Federico Sznajderhaus. Soy un estudiante de Ort, sede Almagro y soy alumno de informática. Estudiaré ingeniería en sistemas en el ITBA. <br></br>
+      Soy muy bueno en todo aquello relacionado a la matemática y a la lógica. Me gusta jugar al basket, los videojuegos y salir de fiesta. <br></br>
+      A la hora de trabajar soy muy organizado, me destaco por mis destalles y perseverancia y poseo habilidades de liderazgo. Siempre intento dar lo mejor de mí, o en todo caso no lo hago. Ofrezco gran compromiso y serenidad.
       
     </div>
   );
